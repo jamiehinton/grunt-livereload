1a15d421e8b740c3fe4c9c0b6a03e9b334e0f363
jhinton <jhinton@technophobia.com>
2013-10-14 16:45:21 +0100

    fleshed out the storage of notes

--- app/model/NoteModel.js
+++ app/model/NoteModel.js
@@ -1,3 +1,19 @@
 Guidebook.service('NoteModel', function(){
-    
+    this.addNote = function(chapterId, noteContent){
+        var now = new Date();
+        var note = {
+            content: noteContent,
+            id: now
+        };
+
+        var chapter = JSON.parse(window.localStorage.getItem(chapterId));
+        if(!chapter){
+            chapter = {
+                id: chapterId,
+                notes: []
+            }
+        }
+        chapter.notes.push(note);
+        window.localStorage.setItem(chapterId, JSON.stringify(chapter));
+    };
 });
